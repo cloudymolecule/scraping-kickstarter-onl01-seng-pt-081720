ee45a3cecc98b608d901439eac4b3540efc3b681
Dario Carlino <baires82@gmail.com>
2020-09-15 13:44:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,12 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   project = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+      projects[project] = {}
+  end
+
+  projects
 end
 
 create_project_hash
