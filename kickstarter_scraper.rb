dcc88d635b4c76f5e59fc43a355bb64f24cca53e
Dario Carlino <baires82@gmail.com>
2020-09-15 13:43:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,8 @@ require "pry"
 def create_project_hash
   html = File.read("fixtures/kickstarter.html")
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+
+  project = {}
 end
 
 create_project_hash
@@ -14,6 +15,5 @@ create_project_hash
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location:
-
-#[9] pry(main)> project.css("ul.project-meta li)
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
